33cae5e22307384edafcce9187af3e8369f50969
theun1c <belovdima1122@yandex.ru>
2026-08-08 01:56:38 +0300

    feat: th base

---
+++ 00-bigO/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+
+Stopwatch stopwatch = new Stopwatch();
+int n = 100;
+
+int Logarithmic(int n)
+{
+    stopwatch.Start();
+    int k = 0;
+    while (n > 0)
+    {
+        n /= 2;
+        k++;
+    }
+    return k;
+}
+
+Logarithmic(n);
+stopwatch.Stop();
+Console.WriteLine($"Log: {stopwatch}ms");
+
+int Linear(int n)
+{
+    stopwatch.Start();
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        k++;
+    }
+    
+    return k;
+}
+
+Linear(n);
+stopwatch.Stop();
+Console.WriteLine($"Linear: {stopwatch}ms");
+
+int Quad(int n)
+{
+    stopwatch.Start();
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            k++;
+        }
+    }
+    
+    return k;
+}
+
+Quad(n);
+stopwatch.Stop();
+Console.WriteLine($"Quad: {stopwatch}ms");
+
+int Exp(int n)
+{ 
+    stopwatch.Start();
+    int k = 0;
+    for (int i = 0; i < Math.Pow(2, n); i++)
+    {
+        k++;
+    }
+    
+    return k;
+}
+
+Exp(n);
+stopwatch.Stop();
+Console.WriteLine($"Exp: {stopwatch}ms");
