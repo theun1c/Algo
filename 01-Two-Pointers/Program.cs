f4a31d24fd48bb0b9462a2bd3f4e4e3a90230c69
theun1c <belovdima1122@yandex.ru>
2026-08-08 19:09:18 +0300

    solved: 125

--- 01-Two-Pointers/Program.cs
+++ 01-Two-Pointers/Program.cs
@@ -38,3 +38,42 @@ void ReverseString(char[] s)
 ReverseString(['H', 'e', 'l', 'l', 'o']);
 
 ReverseString(['H','a','n','n','a','h']);
+
+
+// 125. Valid Palindrome
+// Example 1:
+//
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+//
+// Example 2:
+//
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+//
+// Example 3:
+//
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+//     Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+bool IsPalindrome(string s)
+{
+     char[] letters = s.Where(char.IsLetterOrDigit).Select(char.ToLower).ToArray();
+
+     for (int l = 0, r = letters.Length - 1; l < r; l++, r--)
+     {
+         if(letters[l] != letters[r])
+             return false;
+     }
+     
+     return true;
+}
+
+Console.WriteLine(IsPalindrome("A man, a plan, a canal: Panama"));
+Console.WriteLine(IsPalindrome("Race a car"));
+Console.WriteLine(IsPalindrome(" "));
