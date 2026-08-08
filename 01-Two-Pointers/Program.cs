788359cb0362742306c7340e17fb8a042963f83c
theun1c <belovdima1122@yandex.ru>
2026-08-08 23:22:23 +0300

    solved: 15

--- 01-Two-Pointers/Program.cs
+++ 01-Two-Pointers/Program.cs
@@ -98,16 +98,80 @@ Console.WriteLine(IsPalindrome(" "));
 // Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
 
 // ooomg perfect sol
-int[] TwoSum(int[] numbers, int target) {
+int[] TwoSumSorted(int[] numbers, int target) {
     for (int l = 0, r = numbers.Length - 1; l < r; )
     {
         if (numbers[l] + numbers[r] > target)
             r--;
-        if (numbers[l] + numbers[r] < target)
+        else
             l++;
+        
         if(numbers[l] + numbers[r] == target)
             return new int[] { l, r };
     }
     return new int[] { };
 }
+
+// 15. 3Sum
+// Example 1:
+//
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+//     nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+//     nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+//     The distinct triplets are [-1,0,1] and [-1,-1,2].
+//     Notice that the order of the output and the order of the triplets does not matter.
+//
+//     Example 2:
+//
+// Input: nums = [0,1,1]
+// Output: []
+// Explanation: The only possible triplet does not sum up to 0.
+//
+//     Example 3:
+//
+// Input: nums = [0,0,0]
+// Output: [[0,0,0]]
+// Explanation: The only possible triplet sums up to 0.
+
+IList<IList<int>> ThreeSum(int[] nums)
+{
+    nums = nums.OrderBy(x => x).ToArray();
+
+    IList<IList<int>> result = new List<IList<int>>();
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if( i > 0 && nums[i] == nums[i-1])
+            continue;
+        
+        int target = nums[i] * -1;
+        int left = i + 1;
+        int right = nums.Length - 1;
+        
+        while (left < right)
+        {
+            if(nums[left] + nums[right] > target)
+                right--;
+            else if(nums[left] + nums[right] < target)
+                left++;
+            else
+            {
+                result.Add(new List<int>{nums[left], nums[right], target*-1});
+
+                while (left < right && nums[left] == nums[left+1])
+                    left++;
+                while (left < right && nums[right] == nums[right-1])
+                    right--;
+                
+                left++;
+                right--;
+            }
+        }
+    }
+
+    return result;
+} 
 
