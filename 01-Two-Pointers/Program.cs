82e0590cb62c6369436497a5e90ef5718f90c307
theun1c <belovdima1122@yandex.ru>
2026-08-10 00:18:11 +0300

    solved: 26

--- 01-Two-Pointers/Program.cs
+++ 01-Two-Pointers/Program.cs
@@ -245,3 +245,33 @@ int MaxArea(int[] height)
     
     return result;
 }
+
+// 26. Remove Duplicates from Sorted Array
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Example 2:
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+int RemoveDuplicates(int[] nums)
+{
+    int k = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[k] != nums[i])
+        {
+            k++;
+            nums[k] = nums[i];
+        }
+    }
+    return ++k;
+}
